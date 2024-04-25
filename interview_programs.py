f8c7d1d671c667c779d4a1be8675451ac9dae847
Arun Arunisto <86800553+arun-arunisto@users.noreply.github.com>
2024-04-25 13:48:16 +0530

    Update interview_programs.py

--- interview_programs.py
+++ interview_programs.py
@@ -585,5 +585,17 @@ class Solution:
             else:
                 break
         return result
+"""
+Insertion Sort
+"""
+def insertionSort(li):
+   for i in range(1, len(li)):
+      key = li[i]
+      j = i-1
+      while j>=0 and li[j] > key:
+         li[j+1] = li[j]
+         j-=1
+      li[j+1] = key
+   return li
 
 
