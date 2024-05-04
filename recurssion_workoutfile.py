0e886cacb997df93286f69f5dec141d78668c080
Arun Arunisto <86800553+arun-arunisto@users.noreply.github.com>
2024-05-04 13:38:47 +0530

    Update recurssion_workoutfile.py

--- recurssion_workoutfile.py
+++ recurssion_workoutfile.py
@@ -1,1 +1,10 @@
+#reverse string using recurssion
+def reverse_str(str, n, result=""):
+    if n<0:
+        return result
+    letter = str[n]
+    result+=letter
+    return reverse_str(str, n-1, result)
 
+a = "Arun Arunisto"
+print(reverse_str(a, len(a)-1))
