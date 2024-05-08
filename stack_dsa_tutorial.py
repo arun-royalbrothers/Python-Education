90f998e51be3f83f027403592e6c657a65ea7367
Arun Arunisto <86800553+arun-arunisto@users.noreply.github.com>
2024-05-08 11:51:32 +0530

    Update stack_dsa_tutorial.py

--- stack_dsa_tutorial.py
+++ stack_dsa_tutorial.py
@@ -2,12 +2,24 @@
 # Creating stack using linkedlist
 """
 Stack operations:
-creating node - 
-creating stack - 
+creating node - [Done]
+creating stack - [Done]
 push - To insert an element to last
 pop - To remove/out from the last (LIFO) concept
 peak - To retreive data of the top
-is_empty - to check the stack is empty
+is_empty - to check the stack is empty - [Done]
 length - to get the size of the stack
 traverse - to print the elements in the stack
 """
+
+class Node:
+    def __init__(self, value):
+        self.data = value
+        self.next = None
+
+class Stack:
+    def __init__(self):
+        self.top = None
+        
+    def is_empty(self):
+        return self.top == None
