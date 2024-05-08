a05ae5fecdbe73d281b0b01e712d0ce83044185d
Arun Arunisto <86800553+arun-arunisto@users.noreply.github.com>
2024-05-08 11:45:53 +0530

    Update stack_dsa_tutorial.py

--- stack_dsa_tutorial.py
+++ stack_dsa_tutorial.py
@@ -1,1 +1,11 @@
-
+## 08.05.2024
+# Creating stack using linkedlist
+"""
+Stack operations:
+push - To insert an element to last
+pop - To remove/out from the last (LIFO) concept
+peak - To retreive data of the top
+is_empty - to check the stack is empty
+length - to get the size of the stack
+traverse - to print the elements in the stack
+"""
