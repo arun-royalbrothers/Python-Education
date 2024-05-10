bb35abebb46798578245a0e7dc4c61c03d23b78c
Arun Arunisto <86800553+arun-arunisto@users.noreply.github.com>
2024-05-10 16:43:19 +0530

    Update interview_programs.py

--- interview_programs.py
+++ interview_programs.py
@@ -666,6 +666,25 @@ def reverse_int(num):
         result = result*10+digit
         num//=10
     return result
-
+"""
+LeetCode 500: Keyboard Row
+"""
+class Solution:
+    def findWords(self, words: List[str]) -> List[str]:
+        keyboard = {'q': 1,'w': 1, 'e': 1, 'r': 1, 't': 1,
+                'y': 1,'u': 1, 'i': 1, 'o': 1, 'p': 1,
+                'a': 2, 's': 2, 'd': 2, 'f': 2, 'g': 2,
+                'h': 2, 'j': 2, 'k': 2, 'l': 2, 'z': 3,
+                'x': 3, 'c': 3, 'v': 3, 'b': 3, 'n': 3, 'm': 3}
+        result = []
+        for i in range(len(words)):
+            first_pos = keyboard[words[i][0].lower()]
+            same_row = True
+            for j in range(1, len(words[i])):
+                if keyboard[words[i][j].lower()] != first_pos:
+                    same_row = False
+            if same_row:
+                result.append(words[i])
+        return result
 
 
